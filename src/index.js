3a822c2bbe67cd41353127747a181a5179edd43d
draganet <3417237276@qq.com>
2021-10-14 15:40:13 +0800

    lazy load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import x from './main.js';
 import jpg from './assets/1.jpg'
-console.log(jpg);
 
 console.log(x);
 console.log('hi,webpack');
 
+// 引入图片
 const img = document.querySelector('#app5')
-console.log(img);
 img.innerHTML = `<img src ="${jpg}" width='300px'>`
+
+
+// 懒加载
+const button = document.createElement('button')
+document.querySelector('#app5').appendChild(button)
+button.innerHTML = '懒加载'
+button.onclick = () => {
+    const promise = import('./lazy.js')
+    promise.then((module) => {
+       const fn = module.default()
+       console.log(fn);
+    }, () => {})
+}
